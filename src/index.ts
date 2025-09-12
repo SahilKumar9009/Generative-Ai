6a80698259185ddf5cec8de6e3fa564fa244f401
sahil <sahil.kumar@geeky.dev>
2025-09-12 13:03:01 +0530

    added api's

--- src/index.ts
+++ src/index.ts
@@ -81,6 +81,72 @@ app.post("/api/quiz", async (req: Request, res: Response) => {
   }
 });
 
+//personal assistant for trainner
+
+app.post("/api/personalAssistant", async (req: Request, res: Response) => {
+  const {
+    age,
+    goal,
+    currentHealth,
+    currentWeight,
+    height,
+    weight,
+    activityLevel,
+    exerciseLevel,
+    sleepLevel,
+    dietPlan,
+  } = req.body;
+  try {
+    const prompt = `
+      You are a personal trainer.
+      The user is ${age} years old, with a goal to ${goal}.
+      Current health: ${currentHealth}, weight: ${currentWeight} lbs, height: ${height} inches.
+      Activity level: ${activityLevel}, exercise level: ${exerciseLevel}, sleep level: ${sleepLevel}
+      dietPlan will be vegetarian, vegan, or omnivore.
+      The user has the following diet plan: ${dietPlan}
+      
+      Respond ONLY with a JSON object with these keys:
+      1. workoutPlan: a list of workouts (array of objects with keys: name, sets, reps, rest)
+      2. dietPlan: a list of foods with proper nutrition
+      3. supplementPlan: a list of supplements
+
+      Do NOT include any text outside the JSON object.
+    `;
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash-001",
+
+      contents: [
+        {
+          role: "user",
+          parts: [
+            {
+              text: prompt,
+            },
+          ],
+        },
+      ],
+    });
+
+    function cleanJsonString(rawText: string): string {
+      return rawText
+        .replace(/```json/gi, "") // remove ```json
+        .replace(/```/g, "") // remove ```
+        .trim();
+    }
+    console.log("in the response of the api ", cleanJsonString(response.text!));
+    const aiResponse = cleanJsonString(response.text!);
+    const aiResponseObject = JSON.parse(aiResponse);
+
+    res.status(200).json({
+      data: aiResponseObject,
+    });
+
+    console.log("in the response of the api in the get ai", response);
+  } catch (error) {
+    console.log("in the eroor", error);
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
